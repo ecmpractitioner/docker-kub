3d651f3f23f653444af6dbfbc3715e33f8c2dc29
Manju <ecmpractitioner@gmail.com>
2025-12-02 16:18:42 -0600

    added docker file

--- first_docker_example/helpers.mjs
+++ first_docker_example/helpers.mjs
@@ -1,11 +1,11 @@
 const connectToDatabase = () => {
-  const dummyPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve();
-    }, 1000);
-  });
+    const dummyPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, 1000);
+    });
 
-  return dummyPromise;
+    return dummyPromise;
 };
 
 export default connectToDatabase;
