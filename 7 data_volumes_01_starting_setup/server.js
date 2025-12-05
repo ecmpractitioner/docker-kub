2b4c7e633162dbfbd3e7cbc7daeae56f8bded0db
Manju <ecmpractitioner@gmail.com>
2025-12-04 21:24:55 -0600

    docker volumes - complex topic

---
+++ 7 data_volumes_01_starting_setup/server.js
@@ -0,0 +1,52 @@
+const fs = require('fs').promises;
+const exists = require('fs').exists;
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static('public'));
+app.use('/feedback', express.static('feedback'));
+
+app.get('/', (req, res) => {
+  const filePath = path.join(__dirname, 'pages', 'feedback.html');
+  res.sendFile(filePath);
+});
+
+app.get('/exists', (req, res) => {
+  const filePath = path.join(__dirname, 'pages', 'exists.html');
+  res.sendFile(filePath);
+});
+
+app.post('/create', async (req, res) => {
+  const title = req.body.title;
+  const content = req.body.text;
+
+  const adjTitle = title.toLowerCase();
+
+  const tempFilePath = path.join(__dirname, 'temp', adjTitle + '.txt');
+  const finalFilePath = path.join(__dirname, 'feedback', adjTitle + '.txt');
+
+  await fs.writeFile(tempFilePath, content);
+  exists(finalFilePath, async (exists) => {
+    if (exists) {
+      res.redirect('/exists');
+    } else {
+      // Move the file from temp to feedback directory. But it creates issue when we 
+      //try to move file from container to volume directly.
+      //await fs.rename(tempFilePath, finalFilePath);
+
+      // to avoid the issue, we will use copy and delete method
+      await fs.copyFile(tempFilePath, finalFilePath);
+      await fs.unlink(tempFilePath);
+      res.redirect('/');
+    }
+  });
+});
+
+app.listen(80);
+console.log('Server is running on port 80');
