563b51b4272e6e1b40c46ad187a731328fff547e
Manju <ecmpractitioner@gmail.com>
2025-12-05 18:19:56 -0600

    containers_and_networks_basic

---
+++ 8 containers_and_networks/app.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios').default;
+const mongoose = require('mongoose');
+
+const Favorite = require('./models/favorite');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/favorites', async (req, res) => {
+  const favorites = await Favorite.find();
+  res.status(200).json({
+    favorites: favorites,
+  });
+});
+
+app.post('/favorites', async (req, res) => {
+  const favName = req.body.name;
+  const favType = req.body.type;
+  const favUrl = req.body.url;
+
+  try {
+    if (favType !== 'movie' && favType !== 'character') {
+      throw new Error('"type" should be "movie" or "character"!');
+    }
+    const existingFav = await Favorite.findOne({ name: favName });
+    if (existingFav) {
+      throw new Error('Favorite exists already!');
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+
+  const favorite = new Favorite({
+    name: favName,
+    type: favType,
+    url: favUrl,
+  });
+
+  try {
+    await favorite.save();
+    res
+      .status(201)
+      .json({ message: 'Favorite saved!', favorite: favorite.toObject() });
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong.' });
+  }
+});
+
+app.get('/movies', async (req, res) => {
+  try {
+    const response = await axios.get('https://swapi.dev/api/films');
+    res.status(200).json({ movies: response.data });
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong.' });
+  }
+});
+
+app.get('/people', async (req, res) => {
+  try {
+    const response = await axios.get('https://swapi.dev/api/people');
+    res.status(200).json({ people: response.data });
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong.' });
+  }
+});
+
+//app.listen(3000);
+//The below code connects to MongoDB and starts the server
+//if the local host or the machone on which the container is running does not have MongoDB installed
+// then the connection will fail and the container will not start
+//for this code to work you need to have a MongoDB container running and link it to this container
+
+mongoose.connect(
+  //hardocoded the IP address of the MongoDB container
+  //only works if both containers are running on the same docker network
+  //'mongodb://172.17.0.2:27017/swfavorites',
+  // we can use docker create network my_network
+  // docker run -d --name mongodb --network my_network mongo
+  // docker run -d -p 3000:3000 --name swapp --network my_network swapp_image
+  // then use the mongodb container name as the host
+  'mongodb://mongodb:27017/swfavorites',
+  { useNewUrlParser: true },
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      app.listen(3000);
+    }
+  }
+);
